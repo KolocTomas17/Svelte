59c777d6221041e6c486b7eb7a6aba19dd6c3770
KolocTomas17 <t.koloc@outlook.com>
2023-11-02 19:39:32 +0100

    update

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,80 +1,64 @@
 <script>
-	let count = 1;
+  let count = 1;
 
-	// the `$:` means 're-run whenever these values change'
-	$: one = count * 1;
-	$: oned = doubled * 1;
+  // the `$:` means 're-run whenever these values change'
+  $: one = count * 1;
 
-	$: doubled = count * 2;
-	$: quadrupled = doubled * 2;
+  $: doubled = count * 2;
 
-	$: three = count * 3;
-	$: threed = doubled * 3;
+  $: three = count * 3;
 
-	$: four = count * 4;
-	$: foured = doubled * 4;
+  $: four = count * 4;
 
-	$: five = count * 5;
-	$: fived = doubled * 5;
+  $: five = count * 5;
 
-	$: six = count * 6;
-	$: sixed = doubled * 6;
+  $: six = count * 6;
 
-	$: seven = count * 7;
-	$: sevened = doubled * 7;
+  $: seven = count * 7;
 
-	$: eight = count * 8;
-	$: eighted = doubled * 8;
+  $: eight = count * 8;
 
+  $: nine = count * 9;
 
-	$: nine = count * 9;
-	$: nined = doubled * 9;
+  $: ten = count * 10;
 
-	$: ten = count * 10;
-	$: tened = doubled * 10;
+  function handleClick() {
+    count += 1;
+  }
 
-	function handleClick() {
-		count += 1;
-	}
+  function delClick() {
+    count = 1;
+  }
 </script>
 
 <button on:click={handleClick}>
-	Count: {count}
+  Add number {count}
+</button>
+<button on:click={delClick}>
+  Reset
 </button>
 
-<box>
-	<p>{count}  *  1 = {one}</p>
-	<p>{doubled}  *  1 = {oned}</p>
-
-	<p>{count}  *  2 = {doubled}</p>
-	<p>{doubled}   * 2 = {quadrupled}</p>
-	
-	<p>{count}  *  3 = {three}</p>
-	<p>{doubled}  *  3 = {threed}</p>
-	
-	<p>{count}  *  4 = {four}</p>
-	<p>{doubled} * 4 = {foured}</p>
-	
-	<p>{count}  *  5 = {five}</p>
-	<p>{doubled}  *  5 = {fived}</p>
-</box>
-
 
 <box>
-	<p>{count}  *  6 = {six}</p>
-	<p>{doubled}  *  6 = {sixed}</p>
+  <p>{count} * 1 = {one}</p>
 
-	<p>{count}  *  7 = {seven}</p>
-	<p>{doubled}  *  7 = {sevened}</p>
+  <p>{count} * 2 = {doubled}</p>
 
-	<p>{count}  *  8 = {eight}</p>
-	<p>{doubled}  *  8 = {eighted}</p>
+  <p>{count} * 3 = {three}</p>
 
-	<p>{count}  *  9 = {nine}</p>
-	<p>{doubled}  *  9 = {nined}</p>
+  <p>{count} * 4 = {four}</p>
 
-	<p>{count}  *  10 = {ten}</p>
-	<p>{doubled}  *  10 = {tened}</p>
+  <p>{count} * 5 = {five}</p>
 </box>
 
+<box>
+  <p>{count} * 6 = {six}</p>
 
+  <p>{count} * 7 = {seven}</p>
+
+  <p>{count} * 8 = {eight}</p>
+
+  <p>{count} * 9 = {nine}</p>
+
+  <p>{count} * 10 = {ten}</p>
+</box>
